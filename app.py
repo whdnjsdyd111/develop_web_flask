1cfe241019204aa70a13ce767c7464c45d03bb2b
whdnjsdyd111 <66655578+whdnjsdyd111@users.noreply.github.com>
2021-07-22 23:20:34 +0900

    complete

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ if os.path.isfile('C:\\user_info.data'):
     f.close()
 
 board = [
-    {'title': 'zzz', 'content': 'hi~', 'writer': 'aaa'}
+
 ]
 
+
+# 메인페이지
 @app.route('/')
 @app.route('/index', methods=['GET'])
 def index():
     return render_template('index.html', bd=board)
 
 
+# 로그인
+# GET -> 로그인 페이지
+# POST -> 아이디 조회 후 로그인
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     error = None
     if request.method == 'POST':
-        # if valid_login(request.form['username'],
-        #                request.form['password']):
-        #     return log_the_user_in(request.form['username'])
-        # else:
-        #     error = 'Invalid username/password'
         if request.form['id'] in user_info:
             if request.form['password'] == user_info[request.form['id']]:
                 return redirect(url_for('index'))
@@ -40,6 +40,9 @@ def login():
         return render_template('login.html', error=error)
 
 
+# 회원가입
+# GET -> 회원가입 페이지
+# POST -> 이미 가입됐으면 error, 없으면 가입
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     error = None
@@ -57,19 +60,42 @@ def register():
             return redirect(url_for('login'))
 
 
+# 유저 정보
+# 로그인 하지 않은 상태면 로그인 페이지, 했다면 정보 출력
 @app.route('/user')
 def profile():
     if session.get('id') is None:
         return redirect('/login')
     else:
-        return render_template('userInfo', name=session.get('id'), password=session.get('password'))
+        return render_template('userInfo.html', name=session.get('id'), password=session.get('password'))
 
 
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='John Doe'))
+# 게시판 작성
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    error = None
+    if request.method == 'GET':
+        if session.get('id') is None:
+            error = '로그인 후 이용해주세요.'
+            return redirect(url_for('login'))
+        else:
+            return render_template('write.html', error=error)
+    else:
+        if (request.form['title'] == '') | (request.form['content'] == ''):
+            error = '제목 또는 내용을 입력해 주십시오.'
+            return render_template('write.html', error=error)
+        else:
+            seq = len(board)
+            board.append({'id': seq, 'title': request.form['title'], 'content': request.form['content'],
+                          'writer': session.get('id')})
+            return redirect(url_for('index'))
+
+
+# 게시판
+@app.route('/board/<int:bd_id>')
+def boards(bd_id):
+    return render_template('board.html', bd=board[bd_id])
+
 
 if __name__ == "__main__":
     app.run()
